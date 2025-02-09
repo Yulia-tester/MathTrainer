5cf3463cc4c7801316865647470e05bf40bc6214
Юлия Дегтярева <yuliia.hlavatska@gmail.com>
2025-02-09 11:15:59 -0600

    Add gitignore file

---
+++ MathTrainerLesson/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathTrainerLesson
+//
+//  Created by Юлия Дегтярева on 2025-02-08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
